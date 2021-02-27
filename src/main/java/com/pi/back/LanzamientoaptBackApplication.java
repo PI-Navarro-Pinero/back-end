f63eed3166ac5c535bdd842ca5d28b2f6f4b51e7
Sebastian Navarro <navarrosebastian95@gmail.com>
2021-02-27 18:46:34 -0300

    Spring cargado con algunas dependencias y hello world añadido

--- src/main/java/com/pi/back/LanzamientoaptBackApplication.java
+++ src/main/java/com/pi/back/LanzamientoaptBackApplication.java
@@ -1,4 +1,4 @@
-package com.example.lanzamientoaptback;
+package com.pi.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +8,7 @@ public class LanzamientoaptBackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LanzamientoaptBackApplication.class, args);
+		System.out.println("Hola mundo");
 	}
 
 }
