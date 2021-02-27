2f7ecde5e81e5bf208f670825c0742615a99af7b
Mr. Green <tom-300@hotmail.com>
2021-02-27 18:15:46 -0300

    spring con dependencias añadido

---
+++ src/main/java/com/example/lanzamientoaptback/LanzamientoaptBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.lanzamientoaptback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanzamientoaptBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanzamientoaptBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lanzamientoaptback/LanzamientoaptBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lanzamientoaptback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanzamientoaptBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
