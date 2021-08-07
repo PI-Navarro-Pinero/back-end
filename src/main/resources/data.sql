e62f290d5cbb7c2cf03c8c1c062096cbaec4303c
Sebastian Navarro <navarrosebastian95@gmail.com>
2021-08-07 18:01:25 -0300

    #18 -> refactor scripts and maven for CI

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,13 @@
 -- load roles table
-insert into roles(role) values ('root');
-insert into roles(role) values ('admin');
-insert into roles(role) values ('user');
+insert into roles(id, role)
+values (1, 'root'),
+       (2, 'admin'),
+       (3, 'user');
 
 -- load users table
-insert into users(username, password, roleid) values ('Tomas', 'TomasPinp', 1);
-insert into users(username, password, roleid) values ('Sebastian', 'SebastianPinp', 1);
-insert into users(username, password, roleid) values ('Admin', 'AdminPinp', 2);
-insert into users(username, password, roleid) values ('User', 'UserPinp', 3);
+insert into users(id, username, password, roleid) values (1, 'Tomas', 'TomasPinp', 1);
+insert into users(id, username, password, roleid) values (2, 'Sebastian', 'SebastianPinp', 1);
+insert into users(id, username, password, roleid) values (3, 'Admin', 'AdminPinp', 2);
+insert into users(id, username, password, roleid) values (4, 'User', 'UserPinp', 3);
+
+commit;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,37 +1,18 @@
 -- 'roles' table creation
 create table roles
 (
-    id int auto_increment,
-    role varchar(15) not null
+    id   int primary key auto_increment,
+    role varchar(15) unique not null
 );
-
-create unique index ROLES_ID_UINDEX
-    on roles (id);
-
-create unique index ROLES_ROLE_UINDEX
-    on roles (role);
-
-alter table roles
-    add constraint ROLES_PK
-        primary key (id);
 
 -- 'users' table creation
 create table users
 (
-    id int auto_increment,
-    username varchar(50) not null,
-    password varchar(50) not null,
-    roleid int not null,
+    id       int primary key auto_increment,
+    username varchar(50) unique not null,
+    password varchar(50)        not null,
+    roleid   int                not null,
+
     constraint USUARIOS_ROLES_ID_FK
         foreign key (roleid) references ROLES (ID)
 );
-
-create unique index USUARIOS_ID_UINDEX
-    on users (id);
-
-create unique index USUARIOS_USERNAME_UINDEX
-    on users (username);
-
-alter table users
-    add constraint USUARIOS_PK
-        primary key (id);
