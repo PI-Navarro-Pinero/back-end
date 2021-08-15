cd4d83c47ed74960e23103e48978bc9a1e7d371f
tomaspi <tom-300@hotmail.com>
2021-08-14 23:14:35 -0300

    refactor: use ProcessBuilder

--- src/main/java/com/pi/back/utils/SystemExec.java
+++ src/main/java/com/pi/back/utils/SystemExec.java
@@ -1,25 +1,51 @@
 package com.pi.back.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 @Component
+@Slf4j
 public class SystemExec {
-    public void executeCommand(String cmd) {
+    
+    static final String HOME_PATH = "/home/mrgreen/pinp/";
+
+    public void executeCommand(String cmd[], String weaponId, String actionId) {
+        
+        final String OUTPUT_PATH = HOME_PATH + "outputs/" + weaponId + "/" + actionId;
+
+        // File error = folder.newFile("./.logs/test.log");
+
+        ProcessBuilder procBuilder = new ProcessBuilder(cmd);
+        procBuilder.directory(new File(HOME_PATH));
+        
+        procBuilder.redirectOutput(new File (OUTPUT_PATH));
+
         try {
-            Process p=Runtime.getRuntime().exec(cmd);
-            p.waitFor();
-            BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream()));
-            String line=reader.readLine();
-            while(line!=null) {
-                line=reader.readLine();
-            }
+            Process proc = procBuilder.start();
+
+            int exitValue = proc.waitFor();
+            log.info("Exit code {}", exitValue);
+            log.info("Command: {}", arrayToString(cmd));
+        } catch (InterruptedException | IOException | SecurityException e) {
+            e.printStackTrace();
         }
-        catch(IOException | InterruptedException e1) {
-            e1.printStackTrace();
+    }
+
+    private String arrayToString(String str[]) {
+        StringBuilder builder = new StringBuilder();
+        boolean first = true;
+
+        for (String s : str) {
+            if( !first )
+                builder.append(' ');
+
+            builder.append(s);
+            first = false;
         }
+
+        return builder.toString();
     }
 }
