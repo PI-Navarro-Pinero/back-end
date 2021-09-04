bb638a38417d776efc116fcdc72c13b310d4d951
Sebastian Navarro <navarrosebastian95@gmail.com>
2021-09-03 22:30:31 -0300

    #41 -> controllers are now secured with users's roles and basic auth

--- src/main/java/com/pi/back/db/UsersRepository.java
+++ src/main/java/com/pi/back/db/UsersRepository.java
@@ -1,12 +1,12 @@
 package com.pi.back.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UsersRepository extends JpaRepository<User,Integer> {
+import java.util.Optional;
 
-    @Query(value =  "SELECT U FROM User U WHERE U.id = :userId")
-    User findUser(int userId);
+@Repository
+public interface UsersRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
 }
