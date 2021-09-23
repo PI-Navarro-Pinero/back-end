6b8339c69d1a7dbf639eab985eebf0201ab40462
Sebastian Navarro <navarrosebastian95@gmail.com>
2021-09-23 18:13:32 -0300

    #59 -> cmd execution logic refactor

--- src/main/java/com/pi/back/cmd/CommandManager.java
+++ src/main/java/com/pi/back/cmd/CommandManager.java
@@ -1,45 +1,26 @@
 package com.pi.back.cmd;
 
-import com.pi.back.services.SystemService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@Configuration
+@Slf4j
+@Component
 public class CommandManager {
 
     Pattern COMMAND_MODEL_PATTERN = Pattern.compile("(\\[.*?\\])");
 
-    private final SystemService systemService;
-
-    @Autowired
-    public CommandManager(SystemService systemService) {
-        this.systemService = systemService;
-    }
-
-    @Bean
-    public void executeCommand(/*Integer weaponId, Integer actionId, List<String> queryParamsList*/) {
-        Integer weaponId = 0;
-        Integer actionId = 0;
-        List<String> queryParamsList = List.of("hello world!");
-
-        String commandModel = systemService.retrieveCommandModel(weaponId, actionId);
-        String command = build(commandModel, queryParamsList);
-        systemService.run(command, weaponId.toString(), actionId.toString());
-    }
-
-    public boolean validateCommandWithQueryParametersSize(String commandModel, int queryParamsSize) {
+    public boolean validateUserInput(String commandModel, int queryParamsSize) {
         Matcher commandMatcher = COMMAND_MODEL_PATTERN.matcher(commandModel);
 
         return commandMatcher.results().count() == queryParamsSize;
     }
 
-    public String build(String commandModel, List<String> queryParamsList) {
+    public String buildCommand(String commandModel, List<String> queryParamsList) {
         StringBuilder commandBuilder = new StringBuilder();
         Matcher commandMatcher = COMMAND_MODEL_PATTERN.matcher(commandModel);
 
--- src/main/java/com/pi/back/services/SystemService.java
+++ src/main/java/com/pi/back/services/SystemService.java
@@ -1,19 +1,24 @@
 package com.pi.back.services;
 
+import com.pi.back.cmd.CommandManager;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import javax.naming.directory.InvalidAttributesException;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.FileSystemException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
@@ -27,52 +32,73 @@ public class SystemService {
 
     Map<Integer, Map<Integer, String>> actionsFilesMap;
 
-    public SystemService() {
+    private final CommandManager commandManager;
+
+    @Autowired
+    public SystemService(CommandManager commandManager) {
         actionsFilesMap = new HashMap<>();
+        this.commandManager = commandManager;
+        listFilesFromFolder();
     }
 
-    public String retrieveCommandModel(Integer weaponId, Integer actionId) {
+    public boolean run(Integer weaponId, Integer actionId, List<String> queryParamsList) throws InvalidAttributesException, FileSystemException, ExecutionException {
+        String command = retrieveCommandModel(weaponId, actionId);
+
+        boolean inputIsValid = commandManager.validateUserInput(command, queryParamsList.size());
+
+        if (inputIsValid) {
+            command = commandManager.buildCommand(command, queryParamsList);
+        } else {
+            log.error("");
+            throw new InvalidAttributesException();
+        }
+
+        Process process = execute(command, OUTPUTS_DIR + weaponId);
+        return true;
+    }
+
+    public String retrieveCommandModel(Integer weaponId, Integer actionId) throws InvalidAttributesException {
+        String commandModel = actionsFilesMap.get(weaponId).get(actionId);
+
+        if (commandModel.isEmpty())
+            throw new InvalidAttributesException();
+
+        return commandModel;
+    }
+
+    private Process execute(String command, String outputPath) throws FileSystemException, ExecutionException {
+        File outputFile;
+
         try {
-            return actionsFilesMap.get(weaponId).get(actionId);
+            outputFile = createOutputDirectory(outputPath);
         } catch (Exception e) {
-            log.error("Error when trying to retrieve command model with weaponId {} and actionId {}", weaponId, actionId);
-            return null;
+            log.error("Error creating output path file to '{}'", outputPath);
+            throw new FileSystemException("Error creating output path file");
         }
-    }
 
-    public boolean run(String cmd, String weaponId, String actionId) {
-        final String OUTPUT = OUTPUTS_DIR + weaponId;
+        List<String> splitCommand = List.of(command.split(" "));
 
-        ProcessBuilder procBuilder = new ProcessBuilder(/*cmd*/"ls","-l", "-a", "--author");
+        ProcessBuilder procBuilder = new ProcessBuilder(splitCommand);
 
         try {
-            File action = new File(OUTPUT);
-            action.getParentFile().mkdirs();
-            procBuilder.redirectOutput(action);
-
-            Process proc = procBuilder.start();
-
-            log.info("Command '{}' executed", cmd);
-            return true;
-        } catch (IOException | SecurityException e) {
-            e.printStackTrace();
+            Process proc = procBuilder.redirectOutput(outputFile).start();
+            log.info("Command '{}' executed successfully", command);
+            return proc;
+        } catch (Exception e) {
+            log.error("Error executing command '{}'", command);
+            log.debug("Exception '{}' thrown when executing command '{}': '{}'", e.getClass(), command, e.getCause());
+            throw new ExecutionException(e);
         }
-
-        return false;
     }
 
-    @PostConstruct
-    public void listFilesFromFolder() {
-        AtomicInteger index = new AtomicInteger();
+    private File createOutputDirectory(String outputPath) {
+        File outputFile = new File(outputPath);
+        boolean directoryIsCreated = outputFile.getParentFile().mkdirs();
 
-        try (Stream<Path> paths = Files.walk(Paths.get(ACTIONS_DIR))) {
-            paths
-                    .filter(Files::isRegularFile)
-                    .forEach(file -> actionsFilesMap.put(index.getAndIncrement(),
-                            hashMapFromTextFile(file.getFileName().toString())));
-        } catch (IOException e) {
-            log.error("Error when trying to list files from '{}' directory", ACTIONS_DIR);
-        }
+        if (directoryIsCreated)
+            log.info("Directory '{}' has been created", outputPath);
+
+        return outputFile;
     }
 
     private Map<Integer, String> hashMapFromTextFile(String fileName) {
@@ -92,18 +118,16 @@ public class SystemService {
         return actionsMap;
     }
 
-    private String arrayToString(String[] str) {
-        StringBuilder builder = new StringBuilder();
-        boolean first = true;
+    private void listFilesFromFolder() {
+        AtomicInteger index = new AtomicInteger();
 
-        for (String s : str) {
-            if (!first)
-                builder.append(' ');
-
-            builder.append(s);
-            first = false;
+        try (Stream<Path> paths = Files.walk(Paths.get(ACTIONS_DIR))) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .forEach(file -> actionsFilesMap.put(index.getAndIncrement(),
+                            hashMapFromTextFile(file.getFileName().toString())));
+        } catch (IOException e) {
+            log.error("Error when trying to list files from '{}' directory", ACTIONS_DIR);
         }
-
-        return builder.toString();
     }
 }
