ed7261115b8750f0c3fbe4faec0c26ccea0eee6d
Sebastian Navarro <38895078+navarrosebastian95@users.noreply.github.com>
2021-11-02 22:34:07 -0300

    issue 94 - basic auth for h2 console removed (#95)
    * issue 94 - basic auth for h2 console removed + added strong credentials for it
    
    * issue 94 - easiest pass for h2 console

--- src/main/java/com/pi/back/config/security/WebSecurityConfig.java
+++ src/main/java/com/pi/back/config/security/WebSecurityConfig.java
@@ -31,6 +31,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .authorizeRequests()
+                .antMatchers("/h2-console/**")
+                .permitAll();
+
+        http
                 .csrf().disable()
                 .authorizeRequests()
                 .anyRequest()
