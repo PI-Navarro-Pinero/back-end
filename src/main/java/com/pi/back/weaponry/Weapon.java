c2e8e5c9f0d5a7ebeec0e1ddbec579a4cf2d0ca7
Sebastian Navarro <38895078+navarrosebastian95@users.noreply.github.com>
2021-11-17 21:57:16 -0300

    issue 113 - la aplicacion no inicia si algun campo critico del yaml no es completado (#114)
    * issue 113 - la aplicacion no inicia si algun campo critico del yaml no es completado
    
    * Autogenerated JaCoCo coverage badge
    
    Co-authored-by: CI <ci@users.noreply.github.com>

--- src/main/java/com/pi/back/weaponry/Weapon.java
+++ src/main/java/com/pi/back/weaponry/Weapon.java
@@ -2,14 +2,14 @@ package com.pi.back.weaponry;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.Objects;
 
 @Getter
-@Setter
 @NoArgsConstructor
+@Slf4j
 public class Weapon {
     private String name;
     private String description;
@@ -23,5 +23,33 @@ public class Weapon {
         catch (NullPointerException e) {
             return "";
         }
+    }
+
+    public void setName(String name) {
+        if(name == null) {
+            log.error("Yaml file error: Weapon's name cannot be empty");
+            throw new NullPointerException();
+        }
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        if(description == null) {
+            log.error("Yaml file error: Weapon's description cannot be empty");
+            throw new NullPointerException();
+        }
+        this.description = description;
+    }
+
+    public void setConfigurationFile(String configurationFile) {
+        this.configurationFile = configurationFile;
+    }
+
+    public void setActions(List<String> actions) {
+        if(actions == null || actions.contains(null)) {
+            log.error("Yaml file error: Weapon's actions list cannot be empty");
+            throw new NullPointerException();
+        }
+        this.actions = actions;
     }
 }
