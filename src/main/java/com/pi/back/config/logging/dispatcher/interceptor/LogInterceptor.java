2d328408a42d828bcb93551bb817d6d6d8596303
Sebastian Navarro <38895078+navarrosebastian95@users.noreply.github.com>
2021-11-23 07:46:51 -0300

    issue 127 - all methods are now logged (#128)
    * issue 127 - all methods are now logged
    
    * Autogenerated JaCoCo coverage badge
    
    Co-authored-by: CI <ci@users.noreply.github.com>

--- src/main/java/com/pi/back/config/logging/dispatcher/interceptor/LogInterceptor.java
+++ src/main/java/com/pi/back/config/logging/dispatcher/interceptor/LogInterceptor.java
@@ -2,12 +2,10 @@ package com.pi.back.config.logging.dispatcher.interceptor;
 
 import com.pi.back.config.logging.dispatcher.LoggingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.DispatcherType;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -23,10 +21,7 @@ public class LogInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-
-        if (DispatcherType.REQUEST.name().equals(request.getDispatcherType().name()) && request.getMethod().equals(HttpMethod.GET.name())) {
-            loggingService.logRequest(request, null);
-        }
+        loggingService.logRequest(request, null);
 
         return true;
     }
